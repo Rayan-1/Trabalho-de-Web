0c2e6b56e9c152bc65c0bb2e5d5fce103bfaedc7
Deivison <deivisoneditordevideos@gmail.com>
2022-11-16 20:43:00 -0300

    Criação de usuário e autenticação

--- controllers/UserController.js
+++ controllers/UserController.js
@@ -12,9 +12,67 @@ const generateToken = (id) => {
 };
 
 const register = async (req, res) => {
-  res.send("Registro");
+  const { name, email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (user) {
+    res.status(422).json({ errors: ["Por favor, utilize outro e-mail"] });
+    return;
+  }
+
+  const salt = await bcrypt.genSalt();
+  const passwordHash = await bcrypt.hash(password, salt);
+
+  const newUser = await User.create({
+    name,
+    email,
+    password: passwordHash,
+  });
+
+  if (!newUser) {
+    res
+      .status(422)
+      .json({ errors: ["Houve um erro, por favor tente mais tarde"] });
+
+    return;
+  }
+
+  res.status(201).json({
+    _id: newUser._id,
+    token: generateToken(newUser._id),
+  });
+};
+
+const login = async (req, res) => {
+  const { email, password } = req.body;
+
+  const user = await User.findOne({ email });
+
+  if (!user) {
+    res.status(404).json({ errors: ["Usuário não encontrado."] });
+    return;
+  }
+
+  if (!(await bcrypt.compare(password, user.password))) {
+    res.status(422).json({ errors: ["Senha inválida."] });
+    return;
+  }
+
+  res.status(201).json({
+    _id: user._id,
+    token: generateToken(user._id),
+  });
+};
+
+const getCurrentUser = async (req, res) => {
+  const user = req.user;
+
+  res.status(200).json(user);
 };
 
 module.exports = {
   register,
+  login,
+  getCurrentUser,
 };
