8ff5f50612792f1accc3d48bc9841bf0fadf7fdc
Deivison <deivisoneditordevideos@gmail.com>
2022-11-17 20:35:41 -0300

    Implementação do update user

--- middlewares/userValidations.js
+++ middlewares/userValidations.js
@@ -31,7 +31,21 @@ const loginValidation = () => {
   ];
 };
 
+const userUpdateValidation = () => {
+  return [
+    body("name")
+      .optional()
+      .isLength({ min: 3 })
+      .withMessage("O nome precisa de pelo menos 3 caracteres"),
+    body("password")
+      .optional()
+      .isLength({ min: 6 })
+      .withMessage("A senha precisa ter no mínimo 5 caracteres"),
+  ];
+};
+
 module.exports = {
   userCreateValidation,
   loginValidation,
+  userUpdateValidation,
 };
